e725639872c6218f163ee195b1d471a531166245
chelsian12 <139612214+chelsian12@users.noreply.github.com>
2023-08-03 11:57:55 +0545

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 const App = () => {
+  const movie1 = {
+    Title: "The Lion King",
+    Year: "2019",
+    Runtime: "118 min",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_SX300.jpg",
+  };
+
   return (
     <div>
-
-      <header>
-        dkskzmxk
-      </header>
-      <h1>hello</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti expedita nesciunt delectus commodi officia reprehenderit vel debitis repellat cum aspernatur maiores illo esse voluptatem dolore voluptatibus, at recusandae? Vel perferendis, voluptates neque voluptate exercitationem necessitatibus magni libero, placeat animi harum laborum maxime enim expedita at quaerat magnam ipsum. Optio, possimus.</p>
+      <div className=" border  inline-block bg-gray-600 p-4 m-4">
+        <img src={movie1.Poster} alt="" />
+        <div className="text-center font-serif text-lg text-red-500 capitalize">
+          <h1>{movie1.Title}</h1>
+          <h3>{movie1.Year}</h3>
+          <h4>{movie1.Runtime}</h4>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
