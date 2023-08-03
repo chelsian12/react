af503933aa9943c0951ed7774ebaeddaf7067a39
chelsian12 <139612214+chelsian12@users.noreply.github.com>
2023-08-03 11:57:47 +0545

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react'
-
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+
+      <header>
+        dkskzmxk
+      </header>
+      <h1>hello</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti expedita nesciunt delectus commodi officia reprehenderit vel debitis repellat cum aspernatur maiores illo esse voluptatem dolore voluptatibus, at recusandae? Vel perferendis, voluptates neque voluptate exercitationem necessitatibus magni libero, placeat animi harum laborum maxime enim expedita at quaerat magnam ipsum. Optio, possimus.</p>
+    </div>
   )
 }
 
