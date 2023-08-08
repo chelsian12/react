fc7118b32735f07c1b439c2b72f5cc8d37a3c242
chelsian12 <139612214+chelsian12@users.noreply.github.com>
2023-08-08 10:31:51 +0545

    update

--- src/App.js
+++ src/App.js
@@ -1,46 +1,60 @@
 const App = () => {
-  const movie1 = {
-    Title: "The Lion King",
-    Cast: [
-      "Donald Glover",
-      "Seth Rogen",
-      "Chiwetel Ejiofor",
-      "Alfre Woodard",
-      "Billy Eichner",
-      "John Kani",
-      "John Oliver",
-      "Beyoncé Knowles-Carter",
-      "James Earl Jones",
-    ],
-    Director: "Jon Favreau",
-    Producer: "Jon Favreau, Jeffrey Silver, Karen Gilchrist",
-    Ratings: "6.8",
-    Year: "2019",
-    Runtime: "118 min",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_SX300.jpg",
-  };
+  const numbers = [1, 4, 5, 9, 12, 16, 17, 26];
 
-  return (
-    <div>
-      <div className=" rounded-xl flex-col justify-center  items-center bg-[#1F1F21] p-4  h-600 w-300 ml-80 m-2">
-        <img
-          className="rounded-xl h-350 w-full object-fill"
-          src={movie1.Poster}
-          alt=""
-        />
-        <div className=" text  text-sm text-white p-2 capitalize  break-normal">
-          <h1>Title: {movie1.Title}</h1>
-          <h3>Cast: {movie1.Cast.join(", ")}</h3>
-          <h3>Director: {movie1.Director}</h3>
-          <h3>Producer: {movie1.Producer}</h3>
-          <h3>Ratings: {movie1.Ratings}</h3>
-          <h3>Year: {movie1.Year}</h3>
-          <h3>Runtime: {movie1.Runtime}</h3>
-        </div>
-      </div>
-    </div>
-  );
+  function oddEven(numbers) {
+    const odds = [];
+    const evens = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+      const num = numbers[i];
+      if (num % 2 === 0) {
+        evens.push(num);
+      } else {
+        odds.push(num);
+      }
+    }
+
+    return { odds, evens };
+  }
+
+  function multiplication(numbers) {
+    let multiply = 1;
+    for (let i = 0; i < numbers.length; i++) {
+      multiply *= numbers[i];
+    }
+    return multiply;
+  }
+
+  function totals(numbers) {
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      total += numbers[i];
+    }
+    return total;
+  }
+
+  function minmax(numbers) {
+    let min = Math.min(...numbers);
+    let max = Math.max(...numbers);
+
+    return { min, max };
+  }
+
+  const { odds, evens } = oddEven(numbers);
+  console.log("Odds:", odds);
+  console.log("Evens:", evens);
+
+  const multiply = multiplication(numbers);
+  console.log("multiplication:", multiply);
+
+  const total = totals(numbers);
+  console.log("Total:", total);
+
+  const { min, max } = minmax(numbers);
+  console.log("Minimum:", min);
+  console.log("Maximum:", max);
+
+  return <div></div>;
 };
 
 export default App;
