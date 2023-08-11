901ab067c02ef68155896e212909862d9954c939
chelsian12 <139612214+chelsian12@users.noreply.github.com>
2023-08-11 09:41:22 +0545

    update

--- src/App.js
+++ src/App.js
@@ -1,60 +1,21 @@
+import { Route, Routes } from "react-router"
+import HomePage from "./pages/HomePage"
+import Sample from "./pages/Sample"
+import NotFound from "./pages/NotFound"
+
 const App = () => {
-  const numbers = [1, 4, 5, 9, 12, 16, 17, 26];
 
-  function oddEven(numbers) {
-    const odds = [];
-    const evens = [];
 
-    for (let i = 0; i < numbers.length; i++) {
-      const num = numbers[i];
-      if (num % 2 === 0) {
-        evens.push(num);
-      } else {
-        odds.push(num);
-      }
-    }
 
-    return { odds, evens };
-  }
 
-  function multiplication(numbers) {
-    let multiply = 1;
-    for (let i = 0; i < numbers.length; i++) {
-      multiply *= numbers[i];
-    }
-    return multiply;
-  }
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="sample_page" element={<Sample />} />
+      <Route path="*" element={<NotFound />} />
 
-  function totals(numbers) {
-    let total = 0;
-    for (let i = 0; i < numbers.length; i++) {
-      total += numbers[i];
-    }
-    return total;
-  }
 
-  function minmax(numbers) {
-    let min = Math.min(...numbers);
-    let max = Math.max(...numbers);
-
-    return { min, max };
-  }
-
-  const { odds, evens } = oddEven(numbers);
-  console.log("Odds:", odds);
-  console.log("Evens:", evens);
-
-  const multiply = multiplication(numbers);
-  console.log("multiplication:", multiply);
-
-  const total = totals(numbers);
-  console.log("Total:", total);
-
-  const { min, max } = minmax(numbers);
-  console.log("Minimum:", min);
-  console.log("Maximum:", max);
-
-  return <div></div>;
-};
-
-export default App;
+    </Routes>
+  )
+}
+export default App
