5770e9e2dee3c44df6e52d7cde9543c9f572041e
chelsian12 <139612214+chelsian12@users.noreply.github.com>
2023-08-15 22:16:47 +0545

    update

---
+++ src/pages/web_info/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div class="flex flex-nowrap">
+      <div class="bg-red-300 p-2">Item 1</div>
+      <div class="bg-blue-300 p-2">Item 2</div>
+      <div class="bg-green-300 p-2">Item 3</div>
+    </div>
+
+  )
+}
+
+export default About
