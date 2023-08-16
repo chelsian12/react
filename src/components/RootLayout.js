fca5a08cf286bcb428d152db9757f7e9d8270585
chelsian12 <139612214+chelsian12@users.noreply.github.com>
2023-08-16 19:33:19 +0545

    up

--- src/components/RootLayout.js
+++ src/components/RootLayout.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router'
 import Header from './Header'
+import Footer from './Footer'
 
 export const RootLayout = () => {
   return (
     <>
-<Header/>
-<Outlet/>
-    
+      <Header />
+
+      <Outlet />
+      <Footer />
     </>
   )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -27,15 +27,7 @@ const HomePage = () => {
           </div>
         </div>
       </div>
-      <div className="relative">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000000" fillOpacity="1" d="M0,224L1440,160L1440,320L0,320Z"></path></svg>
 
-
-        <div className="absolute bottom-0 left-2/4 text-white">
-          <h1>Developed By</h1>
-          <h1>Dilip Sahu</h1>
-        </div>
-      </div>
     </div>
   );
 };
